e311920d6d2fc5a012f921e3c1abf1f909ef5473
luiz <luizpauloreiscardosoo@gmail.com>
2023-02-14 11:05:08 -0300

    envio demo 11-02

---
+++ sprint3/terca_feira/script.js
@@ -0,0 +1,34 @@
+//  Declarações (let, const, var)
+const nomeInstrutor = "Luiz"
+let enderecoInstrutor = {rua:'abc' , nr:10}
+let idade = 28
+let eBrasileiro = true
+
+
+
+// tipos
+
+let tipoStrong = "Luiz Paulo endereço 2345656  @#$"     // string
+let tipoNumber = 2  // number
+
+let tipoBoleanTrue = true  // boolean
+let tipoBoleanFalse = false // boolean
+
+let tipoNull = null   // null
+
+let tipoUndefined = undefined  // undefined
+
+console.log(typeof tipoBoleanTrue)
+
+
+let objectA = {rua:'abc' , nrs:[12,13,14]}
+let objetArray = ['luiz','Edu','Lorena']
+
+// boas práticas e palavras reservadas
+
+
+const nomeDoInstrutor = "Luiz"
+let idadeDoInstrutor = 28
+
+let isInstrutor = true
+
